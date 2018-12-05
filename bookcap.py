feb4c55423981e4a86fa5ece7858b18c5a735a44
aka-achea <34272495+aka-achea@users.noreply.github.com>
2018-12-05 16:58:08 +0800

    bc

---
+++ bookcap.py
@@ -0,0 +1,25 @@
+﻿#!/usr/bin/python
+#coding:utf-8
+"""
+SCAN eBOOK
+"""
+
+import time , os
+import pyautogui as auto
+
+
+auto.size()
+width, height = auto.size()
+print(width,height)
+auto.PAUSE = 1
+auto.click(150,150,button='left')
+
+name = "我只想和你说说话"
+
+def scan(name,p):
+    auto.screenshot(name+"_"+p+".png",region=(125,90, 620, 815))
+
+for p in range(1,400):
+    scan(name,str(p))
+    time.sleep(1)
+    auto.typewrite(['right'])
