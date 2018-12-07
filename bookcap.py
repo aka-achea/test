d5ecc0add05ef4d43c5e4b84ad3e4e4f82c94ece
aka-achea <34272495+aka-achea@users.noreply.github.com>
2018-12-07 18:01:44 +0800

    bc

--- bookcap.py
+++ bookcap.py
@@ -7,6 +7,16 @@ SCAN eBOOK
 import time , os
 import pyautogui as auto
 
+path = 'E:\\SIR\\book'
+name = input('Book Name: ')
+pages = input('Total page: ')
+bp = os.path.join(path,name)
+if os.path.isdir(bp) == True:
+    print('Book folder exist')
+else:
+    os.mkdir(bp)
+# print(bp)
+os.chdir(bp)
 
 auto.size()
 width, height = auto.size()
@@ -14,13 +24,21 @@ print(width,height)
 auto.PAUSE = 1
 auto.click(150,150,button='left')
 
-name = input('Book Name: ')
-pages = input('Total page: ')
 
 def scan(name,p):
-    auto.screenshot(name+"_"+p+".png",region=(125,90, 620, 815))
+    png = name+"_"+p+".png"
+    auto.screenshot(png,region=(125,90, 620, 815))
 
-for p in range(1,pages):
+for p in range(1,int(pages)):
     scan(name,str(p))
     time.sleep(1)
     auto.typewrite(['right'])
+
+
+print('finish book capture')
+
+"""
+change log:
+2018.12.7 resize with new screen v1.1
+2017.6.21 build basic screenshot funciton v1.0
+"""
