4d9aa73e420af29336092e08642866e268b8832e
aka-achea <34272495+aka-achea@users.noreply.github.com>
2018-12-24 10:56:11 +0800

    c

--- calc.py
+++ calc.py
@@ -3,10 +3,17 @@
 
 #每天听6小时，中等质量MP3
 
-day = 10
+def mp3calc():
+    day = 10
 
-albumnum = int(day)*6*60/4/12
-size = str(int(day)*6*60/1024)+'GB'
+    albumnum = int(day)*6*60/4/12
+    size = str(int(day)*6*60/1024)+'GB'
 
-print(albumnum)
-print(size)
+    print(albumnum)
+    print(size)
+
+def airticket():
+    pass
+
+if if __name__ == "__main__":
+    pass
