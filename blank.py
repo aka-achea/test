923cb1a18ed49cb8107a48a4ce9dd28ac7c1581a
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-01-01 22:52:41 +0800

    b

--- blank.py
+++ blank.py
@@ -1,29 +1,27 @@
 
 
-import progressbar as pbar
 
-import os,re
-import urllib
+import os,re,configparser
+import urllib,shutil,coloredlogs,logging
 
-def get_pdf(html):
-    """ xxx"""
-    reg = r'href="(.+?\.pdf)">pdf'
-    pdfre = re.compile(reg)
-    pdflist = re.findall(pdfre, html)
-    dir_name = 'COLT2016'
-    if os.path.exists(dir_name) is False:
-        os.mkdir(dir_name)
-    maxrows = len(pdflist)
-    pbar = prgbar.ProgressBar(total=maxrows)
-    for idx, pdfurl in enumerate(pdflist):
-        filename = dir_name + '/' + pdfurl
-        pbar.log('http://jmlr.org/proceedings/papers/v49/' + pdfurl)
-        if os.path.exists(filename) is True:
-            pbar.log('Exist')
-        else:
-            urllib.urlretrieve(
-                'http://jmlr.org/proceedings/papers/v49/' + pdfurl, filename)
-        pbar.update(index=(idx + 1))
-    pbar.finish() 
+confile = 'L:\\MUSIC\\xd.ini'
+config = configparser.ConfigParser()
+config.read(confile)
+topdir = config['arch']['topdir']
+archdir = config['arch']['archdir']
+evadir = config['arch']['evadir']
+coverdir = config['arch']['coverdir']
+musicure = config['arch']['musicure']
+logfile = config['log']['logfile']
+logfilelevel = int(config['log']['logfilelevel'])
 
+inventory =  config['arch']['inventory']
 
+at = '久石譲'
+
+with open(inventory,'r') as f:
+    a = f.readlines()
+    for i in a:
+        m = re.search(at,i.strip())
+        if m:
+            print(i)
