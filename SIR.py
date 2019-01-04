6763669f33dacd2dce1f0310d761a25b33c552ef
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-01-04 16:00:43 +0800

    jj

--- SIR.py
+++ SIR.py
@@ -3,50 +3,62 @@
 
 import time,os
 from docx import Document
+from docx.shared import Inches
 
 SIR = 'E:\\UT\\SIR Blade.docx'
-doc = Document(SIR)
+# doc = Document(SIR)
+
 
 # name = 'ggggg'
 # SN =  'sgeserger'
 # site = 'Singapore'
 # loc = 'IDX2/A24/GSHFRB024A2/Bay79'
-d = time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
-name = input('Server Name: ')
-SN = input('Serial Number: ')
-site = input('Default Shanghai  ')
-if site == '': site = 'Shanghai'
-loc = input('eg:IDX2/A24/GSHFRB024A2/Bay7  ')
+# d = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
+# # name = input('Server Name: ')
+# # SN = input('Serial Number: ')
+# # site = input('Default Shanghai  ')
+# # if site == '': site = 'Shanghai'
+# # loc = input('eg:IDX2/A24/GSHFRB024A2/Bay7  ')
 
 
-"""
-Serial Number:          6CU830G700
-Site:   SHANGHAI
-Area/Building/Room:
-IDX2/A24/GSHFRB024A2/Bay9
-"""
+# """
+# Serial Number:          6CU830G700
+# Site:   SHANGHAI
+# Area/Building/Room:
+# IDX2/A24/GSHFRB024A2/Bay9
+# """
 
-tables = doc.tables
+# tables = doc.tables
 
-t0 = tables[0]
-cName = t0.cell(0,0)
-cName.text = 'System Name:      '+name
-cSN = t0.cell(0,1)
-cSN.text = 'Serial Number:          '+SN
-cSite = t0.cell(1,0)
-cSite.text = 'Site:   '+site
-cloc = t0.cell(1,1)
-cloc.text = 'Area/Building/Room:\n'+loc
+# t0 = tables[0]
+# cName = t0.cell(0,0)
+# cName.text = 'System Name:      '+name
+# cSN = t0.cell(0,1)
+# cSN.text = 'Serial Number:          '+SN
+# cSite = t0.cell(1,0)
+# cSite.text = 'Site:   '+site
+# cloc = t0.cell(1,1)
+# cloc.text = 'Area/Building/Room:\n'+loc
 
-t1 = tables[3]
-cdate = t1.cell(1,1)
-cdate.text = d
+# t1 = tables[3]
+# cdate = t1.cell(1,1)
+# cdate.text = d
 
-try:
-    doc.save(SIR)
-except PermissionError as e:
-    print(e)
-    print('Is file being opened?')
+# # sig = r'E:\UT\signature.png'
+# # s = t1.cell(1,2).add_paragraph()
+# # r = s.add_run()
+# # r.add_picture(sig,width = Inches(0.5), height = Inches(0.5) )
 
-os.startfile(SIR,'print')
+
+# try:
+#     doc.save(SIR)
+# except PermissionError as e:
+#     print(e)
+#     print('Is file being opened?')
+
+# os.startfile(SIR,'print')
+
+
+import comtypes
--- jj.py
+++ jj.py
@@ -5,10 +5,13 @@
 
 from PIL import Image
 import pytesseract,math,os
+from prettytable import PrettyTable 
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-img = 'e:\\t1.png'
-path = 'e:\\'
+
+path = 'e:\\jj'
+threshold = 189
+tdir = os.path.join(path,'t')
 # box = (0,300,655,1230)
 box1 = (0,300,655,530)
 box2 = (0,530,655,760)
@@ -18,6 +21,17 @@ box = [box1,box2,box3,box4]
 # jump = 230
 #(750, 1334)
 # print(im.size)
+ddict = {}
+
+
+def modificate(text):
+    text = str(text)    
+    text = text.replace('，','')   
+    text = text.replace('预约中','')
+    text = text.replace('”','')
+    text = text.strip()
+    return text
+
 
 def initTable(threshold=150):
     table = []
@@ -28,57 +42,59 @@ def initTable(threshold=150):
             table.append(1)
     return table
 
-def format_data(dlist):
-    ddict = {}
-    s = 0
-    for i in range(n):
-        name = dlist[s].split(' ')[0]
-        r = dlist[s+1].split(' ')  #  '4.7540%', '366天'
-        j = [name,r[0][:4],r[-1]]
-        ddict[i] = j
-        s += 3
+def formattable(ddict):
+    t = PrettyTable()
+    t.field_names = ['基金','利率','天数']
+    t.align['基金'] = 'l'
+    t.sortby = '基金'
+    for i in range(len(ddict)):
+        t.add_row(ddict[i+1])
+    print(t)
+
+def readimg(path,threshold):
+    n = 1
+    for i in os.listdir(path):
+        img = os.path.join(path,i)
+        # print(img)
+        im = Image.open(img).convert('L')   
+        for b in range(4):
+            # out = os.path.join(path,str(b)+'.jpg')
+            # print(out)
+            ni = im.crop(box[b])
+            ni = ni.point(initTable(threshold),'1')
+            # ni.save(out,'jpeg')
+            t = pytesseract.image_to_string(ni,lang='chi_sim')
+            # print(t)
+            dlist = []
+            for i in t.split('\n'):
+                if i != '' and i != ' ':
+                    dlist.append(i)
+            # n = len(dlist)
+            name = modificate(dlist[0].split(' ')[0])
+            rate = dlist[1].replace('+','')[:4]
+            for i in range(2,len(dlist)):
+                if '天' in dlist[i]: 
+                    days = dlist[i][:-1]
+                    break
+                else:
+                    days = 1
+            j = [name,rate,days]
+            # print(j) # details
+            ddict[n] = j
+            n += 1
     return ddict
 
 
-
-im = Image.open(img).convert('L')
-
-ddict = {}
-
-for b in range(4):
-    out = os.path.join(path,str(b)+'.jpg')
-    # print(out)
-    ni = im.crop(box[b])
-    ni = ni.point(initTable(195),'1')
-    # ni.save(out,'jpeg')
-    t = pytesseract.image_to_string(ni,lang='chi_sim')
-    # print(t)
-    dlist = []
-    t = t.split('\n')
-    for i in t:
-        if i != '' and i != ' ':
-            dlist.append(i)
-
-    # n = len(dlist)
-    name = dlist[0].split(' ')[0]
-    rate = dlist[1][:4]
-    for i in range(2,len(dlist)):
-        if '天' in dlist[i]: 
-            days = dlist[i][:-1]
-            break
-        else:
-            days = 1
-
-    j = [name,rate,days]
-    ddict[b] = j
-
-
-for i in range(len(ddict)):
-    print(ddict[i])
+print('threshold '+str(threshold))
+ddict = readimg(path,threshold)
+formattable(ddict)
 
 # n = math.floor(/3)
 # ddict = format_data(dlist)
 
-# for i in range(n):
-#     print(ddict[i])
+"""
+change log
+2018.1.4 add pretty table v1.1
+2018.1.3 build core function v1.0
 
+"""
