a7d31766c01f3d45b4faf68ec1e97b215b8338da
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-01-25 16:48:13 +0800

    'update'

--- blank.py
+++ blank.py
@@ -26,75 +26,75 @@ import codecs,fnmatch
 import logging,coloredlogs
 import inspect
 
-def get_funcname():
-    func = inspect.stack()[1][3]
-    mo = str(inspect.stack()[1][1]).split('/')[-1].split('.')[0]
-    mf = mo+'.'+func
+# def get_funcname():
+#     func = inspect.stack()[1][3]
+#     mo = str(inspect.stack()[1][1]).split('/')[-1].split('.')[0]
+#     mf = mo+'.'+func
 
-    lineno = inspect.stack()[1][2]
-    print(lineno)
+#     lineno = inspect.stack()[1][2]
+#     print(lineno)
 
-    return mf
-
-
-class mylogger():
-    def __init__(self,logfile,funcname,logfilelevel=10):
-        logging.basicConfig(level=logfilelevel,filename=logfile,filemode='w',
-                            datefmt='%m-%d %H:%M:%S',
-                            format='%(asctime)s <%(name)s>[%(levelname)s] %(message)s')
-        self.logger = logging.getLogger(funcname)
-        coloredlogs.DEFAULT_LEVEL_STYLES= {
-                                        #'debug': {'color': 'magenta','bold': True},
-                                        'info': {'color': 'green','bold': True},
-                                        'warning': {'color': 'yellow','bold': True},
-                                        'error': {'color': 'red','bold': True},
-                                        'critical': {'color': 'magenta','bold': True}
-                                            }
-        coloredlogs.DEFAULT_LOG_FORMAT = '%(message)s'
-        coloredlogs.install(level='info',logger=self.logger)  
-    def debug(self,msg):
-        self.logger.debug(msg)
-    def info(self,msg):
-        self.logger.info(msg)
-    def warning(self,msg):
-        self.logger.warning(msg)
-    def error(self,msg):
-        self.logger.error(msg)
-    def critical(self,msg):
-        self.logger.critical(msg)
-    def verbose(self,msg):
-        self.logger.debug(msg)
-
-# l = mylogger(logfile,funcname )
+#     return mf
 
 
+# class mylogger():
+#     def __init__(self,logfile,funcname,logfilelevel=10):
+#         logging.basicConfig(level=logfilelevel,filename=logfile,filemode='w',
+#                             datefmt='%m-%d %H:%M:%S',
+#                             format='%(asctime)s <%(name)s>[%(levelname)s] %(message)s')
+#         self.logger = logging.getLogger(funcname)
+#         coloredlogs.DEFAULT_LEVEL_STYLES= {
+#                                         #'debug': {'color': 'magenta','bold': True},
+#                                         'info': {'color': 'green','bold': True},
+#                                         'warning': {'color': 'yellow','bold': True},
+#                                         'error': {'color': 'red','bold': True},
+#                                         'critical': {'color': 'magenta','bold': True}
+#                                             }
+#         coloredlogs.DEFAULT_LOG_FORMAT = '%(message)s'
+#         coloredlogs.install(level='info',logger=self.logger)  
+#     def debug(self,msg):
+#         self.logger.debug(msg)
+#     def info(self,msg):
+#         self.logger.info(msg)
+#     def warning(self,msg):
+#         self.logger.warning(msg)
+#     def error(self,msg):
+#         self.logger.error(msg)
+#     def critical(self,msg):
+#         self.logger.critical(msg)
+#     def verbose(self,msg):
+#         self.logger.debug(msg)
+
+# # l = mylogger(logfile,funcname )
 
 
 
-# if __name__=='__main__': #Usage
-    # funcname = __name__
-    # logfilelevel = 10 # Debug
+
+
+# # if __name__=='__main__': #Usage
+#     # funcname = __name__
+#     # logfilelevel = 10 # Debug
     
-    # l = mylogger(logfile,logfilelevel,funcname)   
+#     # l = mylogger(logfile,logfilelevel,funcname)   
 
-    # l = myconlog()   
+#     # l = myconlog()   
 
-    # l.debug('This is Debug')
-    # l.info('ール・デ')
-    # l.error('error log')
-    # l.warning('warning log')
-    # l.critical("this is a critical message")
-    # l.verbose('vvvvv')
+#     # l.debug('This is Debug')
+#     # l.info('ール・デ')
+#     # l.error('error log')
+#     # l.warning('warning log')
+#     # l.critical("this is a critical message")
+#     # l.verbose('vvvvv')
 
-    # logtest()
+#     # logtest()
 
 
-def function_one():
-    time.sleep(1)
-    print(get_funcname())
+# def function_one():
+#     time.sleep(1)
+#     print(get_funcname())
 
-if __name__ == "__main__":
-    function_one()
+# if __name__ == "__main__":
+#     function_one()
 
 
     # import functools
@@ -137,3 +137,29 @@ if __name__ == "__main__":
     #                 p = os.path.join(dirpath,name)
     #                 s = str(os.path.getsize(p))
     #                 f.write(p +' - '+s+'\n')
+
+
+import asyncio
+
+from aiohttp import web
+
+async def index(request):
+    await asyncio.sleep(0.5)
+    return web.Response(body=b'<h1>Index</h1>')
+
+async def hello(request):
+    await asyncio.sleep(0.5)
+    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'))
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/hello/{name}', hello)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+    print('Server started at http://127.0.0.1:8000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
