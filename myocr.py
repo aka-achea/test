9b744a0a958f1133562bea365d6295d2fc1bd06f
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-04-01 16:46:07 +0800

    'update'

---
+++ myocr.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+#coding:utf-8
+#tested in win
+
+
+from PIL import Image
+import pytesseract
+import os
+
+
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+path = r'E:\jj'
+output = r'E:\jj\out.txt'
+threshold = 120
+
+def initTable(threshold=150):
+    table = []
+    for i in range(256):
+        if i < threshold:
+            table.append(0)
+        else:
+            table.append(1)
+    return table
+
+
+def readimg(imgpath,threshold):
+    # print(img)
+    im = Image.open(imgpath).convert('L')  #灰度图 
+    im.show()
+    ni = im.point(initTable(threshold),'1')
+    # ni.save(out,'jpeg')
+    ni.show()
+    text = pytesseract.image_to_string(ni,lang='chi_sim')
+    # print(t)
+    return text
+
+def main():
+    for j in os.listdir(path):
+        imgpath = os.path.join(path,j)
+        text = readimg(imgpath,threshold)
+        print(text)
+
+# with open(out,'w',newline='',encoding='utf8') as c:
+#     writer = csv.writer(c)
+#     for i in range(len(ddict)):
+#         writer.writerow(ddict[i+1])
+
+if __name__ == "__main__":
+    main()
