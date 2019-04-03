a751e6e515f5a86d5acde4cc80ef03f83c746d98
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-04-03 23:51:54 +0800

    'update'

--- myimg.py
+++ myimg.py
@@ -3,23 +3,31 @@
 # tested in win
 
 from PIL import Image
-import pytesseract
-import os
-import cv2
+import imageio
 import numpy as np
+import os
 
+def gifsplit(pic,outfolder):
+    images = imageio.mimread(pic)
+    #把上面的每帧图片进行保存
+    for i, img in enumerate(images):
+        img = np.asarray(img)
+        imageio.imwrite(os.path.join(outfolder,"%d.png" % i), img)
 
-def gifsplit(pic):
-    gif = Image.open(pic)
-
-    
-    mypalette = gif.getpalette()
-    gif.putpalette(mypalette)
-    im = Image.new('RGBA',gif.size)
-    im.paste(gif)
-    # im.save('image\\{}.png'.format(str(i)))
-
+def gifmake(folder,gifname):
+    fileOrder = sorted([int(os.path.splitext(x)[0]) for x in os.listdir(folder)])
+    # print(fileOrder)
+    frames = []
+    for order in fileOrder:
+        filename = str(order)+ '.png'
+        filePath = os.path.join(folder,filename)
+        frames.append(imageio.imread(filePath))
+    gifpath = os.path.join(folder,gifname)
+    imageio.mimsave(gifpath, frames, 'GIF', duration = 0.1)
 
 if __name__ == "__main__":
     pic = r'M:\MyProject\ocr\t.gif'
-    gifsplit(pic)
+    outfolder = r'M:\MyProject\ocr'
+    # gifsplit(pic,outfolder)
+    gifname = 'g.gif'
+    gifmake(outfolder,gifname)
