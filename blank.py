0047c2495bc9a790f1864542be4745f5bbd255a7
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-04-13 23:01:08 +0800

    'update'

--- blank.py
+++ blank.py
@@ -14,49 +14,12 @@ from flask import Flask
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
+def find_album(album):
+    with open(albumlist,'r',encoding='utf-8') as f:        
+        for i in f.readlines():
+            if album == i.strip():
+                return True
 
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-# db.drop_all()
-# db.create_all()
-# admin = Role(name='Adm')
-# mod = Role(name='Moderator')
-
-# u_joh = User(username='joh',role=admin)
-
-
-
-# db.session.add_all([admin,mod,u_joh])
-# db.session.commit()
-# print(admin.name)
-# print(mod.id)
-# print(u_joh.id)
-
-print(Role.query.all())
-User.query.all()
+albumlist = r'L:\Music\album.txt'
+a = find_album('The Roots - 2010 - How I Got Over')
+print(a)
