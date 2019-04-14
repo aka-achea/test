e545a5e1a5e80d1cd1f481983d2dcf840dff3051
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-04-14 23:01:36 +0800

    'update'

--- blank.py
+++ blank.py
@@ -14,12 +14,20 @@ from flask import Flask
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
-def find_album(album):
-    with open(albumlist,'r',encoding='utf-8') as f:        
-        for i in f.readlines():
-            if album == i.strip():
-                return True
 
-albumlist = r'L:\Music\album.txt'
-a = find_album('The Roots - 2010 - How I Got Over')
-print(a)
+
+import win32con
+import win32clipboard as wincld
+
+
+def get_text():
+    wincld.OpenClipboard()
+    text_result = wincld.GetClipboardData(win32con.CF_UNICODETEXT)
+    # wincld.EmptyClipboard()
+    wincld.CloseClipboard()
+    return text_result
+
+
+
+# read clipboard
+print(get_text())
