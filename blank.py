6d47b65feef30edd98687afeaa064ef2ae8cb6cc
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-04-20 23:01:11 +0800

    'update'

--- blank.py
+++ blank.py
@@ -11,7 +11,6 @@ import codecs,fnmatch
 import logging,coloredlogs
 import inspect
 from flask import Flask
-from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -20,14 +19,12 @@ import win32con
 import win32clipboard as wincld
 
 
-def get_text():
-    wincld.OpenClipboard()
-    text_result = wincld.GetClipboardData(win32con.CF_UNICODETEXT)
-    # wincld.EmptyClipboard()
-    wincld.CloseClipboard()
-    return text_result
 
 
+txt = 'a1a1a1.live | a2a2a2.live | a3a3a3.live | gygygy.live | kgkgkg.live | nenene.live | bababa.live | bububu.live | gagaga.live | hehehe.live | 174.127.195.66 | 174.127.195.69 | 174.127.195.98 | 174.127.195.102 | 174.127.195.176 | 174.127.195.178 | 174.127.195.201 | 174.127.195.183 | 174.127.195.186 | 174.127.195.188 | 174.127.195.173 | 174.127.195.187 | 174.127.195.182 | 174.127.195.184 | 174.127.195.171 | 174.127.195.166 | 174.127.195.226 | 67.220.90.10 | 67.220.90.4 | 67.220.90.20 | 67.220.90.15 | 174.127.195.213 | 174.127.195.190 | 174.127.195.198 | 174.127.195.205 | a.1u2u3u4u.com'
 
-# read clipboard
-print(get_text())
+def buildiplist(txt):
+    iplist = re.split(' \| ',txt)
+    return iplist
+
+print(buildiplist(txt))
