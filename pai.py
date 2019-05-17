9cbdda225155a33da96dddc8c6d89af5d3a3c6d0
aka-achea <cjyrb@hotmail.com>
2019-05-17 17:01:57 +0800

    'update'

--- pai.py
+++ pai.py
@@ -5,5 +5,9 @@ path = r'E:\UT'
 f = 'pai.xlsx'
 
 df = pd.read_excel(os.path.join(path,f),'raw')
+df.rename(columns{'拍卖时间':'date','投放数量':'number',\
+                   '警示价':'start','最低成交价':'low',
+                   '平均成交价':'mean','投标人数':'enrollment'
 
+})
 print(df)
