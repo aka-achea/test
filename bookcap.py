18e515dfa2725c3ed6ccea7fd550b1250130bb48
aka-achea <cjyrb@hotmail.com>
2019-07-19 16:51:00 +0800

    'update'

--- bookcap.py
+++ bookcap.py
@@ -19,8 +19,8 @@ def scan_kindle(name,p):
 def scan_bookshelf(name,p): 
     '''in middle monitor'''
     png = name+"_"+str(p)+".png"
-    auto.screenshot(png,region=(15,460,1040,1365))  # for bookshelf
-    time.sleep(0.2)
+    auto.screenshot(png,region=(15,450,1040,1360))  # for bookshelf
+    # time.sleep(0.2)
     auto.keyDown('ctrl')
     auto.keyDown('pagedown')
     auto.keyUp('ctrl')
@@ -29,35 +29,39 @@ def scan_bookshelf(name,p):
  
 
 if __name__ == "__main__":
+    try:
+        path = r'E:\daka'
+        # name = input('Book Name: ')
+        # pages = input('Total page: ')
+        name = 'A5.5'
+        pages = '880'
 
-    path = r'E:\daka'
-    # name = input('Book Name: ')
-    # pages = input('Total page: ')
-    name = 'TE'
-    pages = '103'
+        bp = os.path.join(path,name)
+        if os.path.isdir(bp):
+            print('Book folder exist')
+        else:
+            os.mkdir(bp)
+        os.chdir(bp)
 
-    bp = os.path.join(path,name)
-    if os.path.isdir(bp):
-        print('Book folder exist')
-    else:
-        os.mkdir(bp)
-    os.chdir(bp)
+        width, height = auto.size()
+        # print(width,height)
+        auto.PAUSE = 1
+        # auto.click(150,150,button='left') # for kindle
+        auto.click(800,800,button='left') # for bookshelf
 
-    width, height = auto.size()
-    print(width,height)
-    auto.PAUSE = 1
-    # auto.click(150,150,button='left') # for kindle
-    auto.click(800,800,button='left') # for bookshelf
+        for p in range(572,int(pages)):
+            scan_bookshelf(name,p)
 
-    for p in range(68,int(pages)):
-        scan_bookshelf(name,p)
+        print('finish book capture')
 
-    print('finish book capture')
+    except KeyboardInterrupt:
+        print("Stop")
 
 
 
 """
 change log:
+2019.7.19 add bookshelf for aws v1.2
 2018.12.7 resize with new screen v1.1
 2017.6.21 build basic screenshot funciton v1.0
 """
