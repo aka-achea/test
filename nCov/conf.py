ef4290230e917a9c9297127f49bc8d9bd4a056a7
aka-achea <cjyrb@hotmail.com>
2020-03-12 13:41:26 +0800

    'update'

--- nCov/conf.py
+++ nCov/conf.py
@@ -4,18 +4,23 @@
 from configparser import ConfigParser
 import os
 
+
 baiduconf = r'M:\GH\_pri\baidu.ini'
-config = ConfigParser()
-config.read(baiduconf)
-ak_geo = config.get('BaiduMap','ak_2020_srv') 
-sk_geo = config.get('BaiduMap','sk_2020_srv')
-ak_web = config.get('BaiduMap','ak_nCovApi_browser')
-ak_dev = config.get('BaiduMap','ak_dev_browser')
+
+def readconf(baiduconf):
+    config = ConfigParser()
+    config.read(baiduconf)
+    ak_geo = config.get('BaiduMap','ak_2020_srv') 
+    sk_geo = config.get('BaiduMap','sk_2020_srv')
+    ak_web = config.get('BaiduMap','ak_nCovApi_browser')
+    ak_dev = config.get('BaiduMap','ak_dev_browser')
+    return ak_dev,ak_geo,ak_web,sk_geo
 
 bucket = '2020-1301184162'
 dest = 'render.html'
 logbucket = 'log-1301184162'
-workpath = r'M:\MyProject\nCov'
+# workpath = r'M:\MyProject\nCov'
+workpath = r'C:\Business\GitHub\nCov'
 qconf = os.path.join(workpath,'qcloud.ini')
 outfile = os.path.join(workpath,dest)
 logdir = os.path.join(workpath,'log')
