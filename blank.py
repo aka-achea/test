21d513d6f5d6bbfbc256a5a4656789ecfb82b850
aka-achea <cjyrb@hotmail.com>
2020-05-18 23:01:21 +0800

    'update'

--- blank.py
+++ blank.py
@@ -5,11 +5,13 @@ from myfs import g_fsize,jdump,jload
 from itertools import product
 from pprint import pprint
 
+q='magnet:?xt=urn:btih:2UVRVPWZV7FMBD3GULCY4TMNML24NSAE&dn=%e5%8d%87%e7%ba%a7%2e1080p%2eBD%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97%5b%e6%9c%80%e6%96%b0%e7%94%b5%e5%bd%b1www%2e66ys%2etv%5d%2emp4&tr=udp%3a%2f%2f9%2erarbg%2eto%3a2710%2fannounce&tr=udp%3a%2f%2f9%2erarbg%2eme%3a2710%2fannounce&tr=http%3a%2f%2ftr%2ecili001%2ecom%3a8070%2fannounce&tr=http%3a%2f%2ftracker%2etrackerfix%2ecom%3a80%2fannounce&tr=udp%3a%2f%2fopen%2edemonii%2ecom%3a1337&tr=udp%3a%2f%2ftracker%2eopentrackr%2eorg%3a1337%2fannounce&tr=udp%3a%2f%2fp4p%2earenabg%2ecom%3a1337'
 
-# def unquote_url(url):
-#     from urllib import parse
-#     return parse.unquote(url)
+def unquote_url(url):
+    from urllib import parse
+    return parse.unquote(url)
 
+print(unquote_url(q))
 # from bs4 import BeautifulSoup
 # from hyper import HTTPConnection
 # from urllib.parse import urlparse,parse_qs
@@ -34,97 +36,3 @@ from pprint import pprint
 # html = resp.read().decode('utf-8')
 # print(html)
 
-from PIL import Image
-import math,os ,csv,codecs
-from prettytable import PrettyTable 
-# from pathlib import PureWindowsPath as pp
-import time
-import openpyxl
-from pprint import pprint
-
-from mystr import batchremovestr
-from myocr import init_client,ocr_baidu
-
-'''
-1. iPhone wx screen shot
-2. Picsew 长截图
-3. wx 转原图
-4. Baidu OCR
-'''
-
-
-path = r'M:\MyProject\JiJin'
-# xls = r'M:\MyProject\JiJin\t.xlsx'
-xls = r'N:\Doc\财务统计.xlsx'
-
-
-
-
-class jijin():
-    def __init__(self,name):
-        self.name = name
-        self.rate = None
-        self.days = None
-        self.amount = None
-        self.match = None
-
-
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
-
-
-def modificate(text):
-    rlist = ['假期收益已提前发放','预约中',',','支持','零钱通','买入','净值型','智能','NEW','灵活申赎','随买随取最快当到账','HOT']
-    return batchremovestr(rlist,text)
-
-def readjj(ocrclient,jdict,path):
-    '''Crop the image every 271 pix'''
-    img = os.path.join(path,'j.png')
-    im = Image.open(img).convert('L') 
-    try:    
-        # ni.show()
-        im.save('tmp.png', 'PNG')
-        data = get_file_content('tmp.png')
-        txtlist = ocr_baidu(ocrclient,data)
-        pprint(txtlist)
-        # name = modificate(txtlist[0])
-        # rateday = txtlist[1].split('%')
-        # rate = rateday[0]
-        # if '天' in rateday[1]:
-        #     days = rateday[1].replace('天','')
-        # elif '个月' in rateday[1]:
-        #     days = rateday[1].replace('个月','')
-        # else:
-        #     days = txtlist[2].replace('天','')
-        # if modificate(days) == '':
-        #     days = 1
-        # if name not in jdict.keys():
-        #     j = jijin(name)
-        #     jdict[name] = j
-        # jdict[name].rate = rate
-        # jdict[name].days = days
-        # if txtlist == []:
-        #     break
-        # n += 1
-        # time.sleep(1)
-    except:
-        raise
-    # pprint(jdict) # details
-    return jdict
-
-def formattable_jj(jdict):
-    t = PrettyTable()
-    t.field_names = ['基金','天数','利率','数量','Match']
-    t.align['基金'] = 'l'
-    t.sortby = '基金'
-    for k in jdict.keys():
-        jj = jdict[k]
-        t.add_row([ jj.name,jj.rate,jj.days,jj.amount,jj.match ])
-    print(t)
-
-
-ocr = init_client()
-jdict = {}
-jdict = readjj(ocr,jdict,path)
-# formattable_jj(jdict)
